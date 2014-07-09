f99b0c003953477d73981da9e69d72620fa3a403
Alex <alex.ehrensberger@gmail.com>
2014-07-09 16:05:12 -0400

    Bug fixes

--- app/view/SearchPanel.js
+++ app/view/SearchPanel.js
@@ -8,23 +8,28 @@ Ext.define('MyApp.view.SearchPanel', {
 		modal: true,
 		hideOnMaskTap: true,
 		top: 0,	
-		left: 0,
-		showAnimation: 'slideIn',
+		
+		showAnimation: {
+			type: 'slideIn',
+			direction: 'down'
+		},
 		hideAnimation: {
 			type: 'slideOut',
-			direction: 'right',
+			direction: 'up',
 		},
+				
 		items: [
 			{
 				xtype: 'toolbar',
 				docked: 'top',
+
 				
 				items: [
 					{
 						xtype: 'searchfield',
 						placeHolder: 'Search...',
 						itemId: 'searchBox',
-						width: window.innerWidth,						
+						width: window.innerWidth - 10					
 					}
 				]
 			}
